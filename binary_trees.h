fa7146530075a871b62f66b2ac1a9b4a4930b54f
Wilder Rincon <1588@holbertonschool.com>
2020-07-23 18:59:52 -0500

    Adding changes file binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -24,7 +24,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /** Print functions */
 void binary_tree_print(const binary_tree_t *);
@@ -32,7 +34,6 @@ void binary_tree_print(const binary_tree_t *);
 /** Functions of tasks */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-void print_num(int n);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
