37fbdc7873822dac323d837f50df999c7fc1d70d
Esteban1891 <esteban.delahoz15@gmail.com>
2020-07-24 02:38:39 -0500

    add task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,8 +13,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left != NULL || tree->right != NULL)
+	if (tree->left != NULL)
 		left_count = binary_tree_height_2(tree->left);
+	if (tree->right != NULL)
 		right_count = binary_tree_height_2(tree->right);
 
 	return (left_count - right_count);
